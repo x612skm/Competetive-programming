e7ee5af8fb79a99b9cf55c5c7fbe1997d5a0a4e4
x612skm <soubhikmitra98@gmail.com>
2021-09-18 22:41:02 +0530

    n-meetings

---
+++ n-meetings.cpp
@@ -0,0 +1,28 @@
+// the idea is to get sorted the all the meetings according to their finishing times 
+//after getting sorted in their finishing times they are all managed according to the direction
+//once the meeting done new meeting cannot be start from as soon as the old meeting 
+//there must be a gap between two meetings return the number of meetings
+
+
+struct meeting{
+	
+}
+//making a comparator for the following 
+bool comparator(struct meeting m, meeting one){
+	if(m1.end<m2.end) return true;
+	else if(m1.end>m2.end) return false;
+	else if(m1.pos<m2.pos) return true;
+	return false;
+
+}
+
+void maxMeetings(int s[], int l[], int e[])
+{
+	struct meeting meet[n];
+
+	//creating an iterator to assign
+	for(int i=0; i<n ; i++)
+	{
+		//iterating to the last element 
+	}
+}
