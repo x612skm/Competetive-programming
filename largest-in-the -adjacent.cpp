cc53c5a3851aad531b5ed4c3e9201ce87caa1003
x612skm <soubhikmitra98@gmail.com>
2021-10-08 20:46:23 +0530

    oa sheet

---
+++ largest-in-the -adjacent.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int test;
+	cin>> test;
+	int n = test.size();
+	for(int i=0; i<n; i++)
+	{
+		//take the array as the input 
+		
+	}
+	return;
+}
---
+++ ternery-strings.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+char buf[200043];
+
+int main()
+{
+	int t;
+	scanf("%d", &t);
+	for(int i=0; i<t; i++)
+	{
+		scanf("%s", buf);
+		string s = buf;
+		int ans = int(1e9);//initialisation of int ^6
+
+		int n = s.size();
+
+		vector<pair<char, int>> c;
+
+		//iterate
+		for(auto x : s)
+		{
+			if(c.empty() || c.back().first != x)
+				c.push_back(make_pair(x,1));
+			else
+				c.back().second++;
+		}
+
+		int m = c.size();
+
+		for(int i=1; i<m-1; i++)
+			if(c[i-1].first != c[i+1].first)
+				ans = min(ans, c[i].second + 2);
+
+		if(ans>n)
+			ans = 0;
+
+		printf("%d\n",ans);
+	}
+}
